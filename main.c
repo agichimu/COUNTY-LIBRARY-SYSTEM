e1ebe8743e50559c05ca3ba77b132df9926b3460
alexandergichimu <97959452+alexandergichimu@users.noreply.github.com>
2022-03-15 19:52:17 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -7,7 +7,28 @@ C99 COMPILER
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
+struct Patron{
+    char name[100];
+    char email[50];
+    char password[30];
+    int is_staff;
+};
+
+void add_patron() {
+    struct Patron patron;
+    printf("Enter name: ");
+    getchar();
+    gets(patron.email);
+    printf("Enter Email: ");
+    gets(patron.email);
+    printf("Enter initial password: ");
+    gets(patron.email);
+    printf("Enter 1 if staff 0 otherwise");
+    scanf("%d" ,&patron.is_staff);
+    printf("$s added\n" ,patron.name);
+}
 int menu()
 {
    int action;
@@ -18,14 +39,34 @@ int menu()
    printf("4.Add New Books!\n");
    printf("Your Action!\n");
    scanf("%d",&action);
+   if(action<1|| action > 4) {
+        printf("invalid Action. Try again\n");
+   }
    return action;
-
 }
+void execute_action(int action) {
+    switch(action) {
+case 1:
+    add_patron();
+    break;
+case 2:
+    printf("list of all patrons \n");
+    break;
+case 3:
+    printf("list of all books\n");
+    break;
+case 4:
+    printf("adding a new book\n");
+    break;
+    default: printf("invalid action .\n");
+    }
+}
+
 int main()
 {
    printf("COUNTY LIBRARY SYSTEM!\n");
    printf("WELCOME PROGRAMMER ALEXANDER!\n");
-   printf("You selected action %d",menu());
+   execute_action(menu());
    return 0;
 }
 
